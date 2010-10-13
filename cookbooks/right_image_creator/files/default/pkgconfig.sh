7b30ea706659bf3da0d085cb4ce363ef29cd0859
Martin Rhoads <martin@rightscale.com>
2010-10-13 11:37:45 -0700

    removing stray dirs and adding rdoc

--- cookbooks/right_image_creator/files/default/pkgconfig.sh
+++
@@ -1,1 +0,0 @@
-export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/local/lib/pkgconfig
--- cookbooks/right_image_creator/solo.rb
+++
@@ -1,4 +0,0 @@
-file_cache_path "#{ENV['HOME']}/Dropbox/dev/my_cookbooks"
-cookbook_path [ "#{ENV['HOME']}/Dropbox/dev/my_cookbooks/cookbooks" ]
-
-
