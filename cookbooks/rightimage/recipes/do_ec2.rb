7634a59ffbc4e1ca832449dcde531f31cd963b68
Jeremy Deininger <jeremy@rightscale.com>
2010-11-01 00:34:58 -0700

    [workflow] adding do_ebs section after s3 build

--- cookbooks/rightimage/recipes/do_ec2.rb
+++ cookbooks/rightimage/recipes/do_ec2.rb
@@ -160,8 +160,113 @@ bash "bundle_upload_ec2" do
   echo y | ec2-upload-bundle -b #{node[:rightimage][:image_upload_bucket]} -m "#{node[:rightimage][:mount_dir]}"_temp/#{image_name}.manifest.xml -a #{node[:rightimage][:aws_access_key_id]} -s #{node[:rightimage][:aws_secret_access_key]} --retry --batch
   
   echo registering... 
-  ec2-register #{node[:rightimage][:image_upload_bucket]}/#{image_name}.manifest.xml -K  /tmp/AWS_X509_KEY.pem -C  /tmp/AWS_X509_CERT.pem -n "#{image_name}" --url #{node[:rightimage][:ec2_endpoint]}
-  
+  image_out_s3=`ec2-register #{node[:rightimage][:image_upload_bucket]}/#{image_name}.manifest.xml -K  /tmp/AWS_X509_KEY.pem -C  /tmp/AWS_X509_CERT.pem -n "#{image_name}" --url #{node[:rightimage][:ec2_endpoint]} `
+
+  echo "Doing EBS"
+
+# switch to a random directory for the build
+  random_dir="/mnt/$RANDOM"
+  mkdir $random_dir
+  cd $random_dir
+  ebs_mount=${random_dir}/ebs_mount
+    mkdir -p $ebs_mount
+# This /mnt/image is where the image creator creates the image
+  image_mount=/mnt/image
+
+## calculate ec2 region
+  length=`echo -n #{node[:ec2][:placement_availability_zone]} | wc -c`
+  length_minus_one=$((length -1))
+  region=`echo  #{node[:ec2][:placement_availability_zone]} | cut -c -$length_minus_one`
+
+    ## create EBS volume
+  vol_out=`ec2-create-volume \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --size 10 \
+    --url #{node[:rightimage][:ec2_endpoint]} \
+    --availability-zone #{node[:ec2][:placement_availability_zone]} `
+
+# parse out volume id
+  vol_id=`echo -n $vol_out | awk '{ print $2 }'`
+
+## attach an EBS volume here
+  ec2-attach-volume $vol_id \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --device /dev/sdj \
+    --url #{node[:rightimage][:ec2_endpoint]} \
+    --instance #{node[:ec2][:instance_id]} 
+
+## loop and wait for volume to become available
+  while [ 1 ]; do 
+    vol_status=`ec2-describe-volumes $vol_id  --private-key /tmp/AWS_X509_KEY.pem --cert /tmp/AWS_X509_CERT.pem --url #{node[:rightimage][:ec2_endpoint]}`
+    if `echo $vol_status | grep -q "attached"` ; then break; fi
+    sleep 1
+  done 
+
+  sleep 10
+## format and mount volume
+  mkfs.ext3 -F /dev/sdj
+  mount /dev/sdj $ebs_mount
+
+## mount EBS volume, rsync, and unmount ebs volume
+  rsync -a $image_mount/ $ebs_mount/
+  umount $ebs_mount
+
+## snapshot the ebs volume and save the snapshot id
+  snap_out=`ec2-create-snapshot $vol_id \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --url #{node[:rightimage][:ec2_endpoint]} \
+    --description "This snapshot will be used to create #{image_name}"`
+    
+# parse out snapshot id
+  snap_id=`echo -n $snap_out | awk '{ print $2 }'`
+
+## loop and wait for snapshot to become available
+  while [ 1 ]; do 
+    snap_status=`ec2-describe-snapshots $snap_id --private-key /tmp/AWS_X509_KEY.pem --cert /tmp/AWS_X509_CERT.pem --url #{node[:rightimage][:ec2_endpoint]} `
+    if `echo $snap_status | grep -q "completed"` ; then break; fi
+    sleep 5
+  done 
+
+  image_out_ebs=`ec2-register \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --region $region \
+    --url #{node[:rightimage][:ec2_endpoint]}\
+    --architecture #{node[:rightimage][:arch]} \
+    -b "sdb=ephemeral0" \
+    --description "#{image_name}_EBS" \
+    --name "#{image_name}_EBS" \
+    --snapshot $snap_id \
+    $kernel_opt \
+    $ramdisk_opt \
+    --root-device-name /dev/sda1 `
+ 
+ # parse out image id
+  image_id_s3=`echo -n $image_out_s3 | awk '{ print $2 }'`
+  image_id_ebs=`echo -n $image_out_ebs | awk '{ print $2 }'`
+
+  echo "$image_id_s3,$image_id_ebs" > /tmp/tag_these_images.csv
+
+## detach volume
+  ec2-detach-volume $vol_id \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --region $region \
+    --url #{node[:rightimage][:ec2_endpoint]} \
+    --force
+
+  sleep 10
+
+## delete volume
+  ec2-delete-volume $vol_id \
+    --private-key /tmp/AWS_X509_KEY.pem \
+    --cert /tmp/AWS_X509_CERT.pem \
+    --url #{node[:rightimage][:ec2_endpoint]} \
+    --region $region 
+ 
   #remove keys
   rm -f /tmp/AWS_X509_KEY.pem
   rm -f  /tmp/AWS_X509_CERT.pem
