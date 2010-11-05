8a2509aad74e61e91b8a12b2e32b8cd9be15d918
Jeremy Deininger <jeremy@rightscale.com>
2010-11-05 01:48:32 -0700

    [workflow] adding mci creation proof of concept

--- cookbooks/rightimage/recipes/do_ec2.rb
+++ cookbooks/rightimage/recipes/do_ec2.rb
@@ -1,6 +1,9 @@
 class Chef::Resource::Bash
   include RightScale::RightImage::Helper
 end
+class Chef::Resource::RubyBlock
+  include RightScale::RightImage::Helper
+end
 
 # configure an image for a particular cloud. 
 # we need to:
@@ -282,8 +285,8 @@ if node[:rightimage][:cloud] == "ec2"
   end
   r.run_action(:install)
   Gem.clear_paths
-
-  # Tag the images that were just created
+  
+ # Tag the images that were just created
   ruby_block "tag the images" do
     block do
       @cloud_names = { "us-east" => "1", "eu-west" => "2", "us-west" => "3","ap-southeast" => "4"}
@@ -291,18 +294,47 @@ if node[:rightimage][:cloud] == "ec2"
       @cloud_names.each do |cloud_name, cloud_id|
         @region = cloud_id if node[:ec2][:placement_availability_zone].include?(cloud_name)
       end
+require 'fileutils'
+restcon_config =<<EOF
+---
+:pass: #{node[:rest_connection][:pass]}
+:user: #{node[:rest_connection][:user]}
+:api_url: #{node[:rest_connection][:api_url]}
+:common_headers: 
+  X_API_VERSION: "1.0"
+EOF
+ENV['REST_CONNECTION_LOG'] = "/tmp/rest_connection.log"
+	restcondir = ::File.join(::File.expand_path("~"), ".rest_connection")
+	FileUtils.mkdir_p(restcondir)
+      ::File.open(File.join(restcondir, "rest_api_config.yaml"),"w") {|f| f.write(restcon_config)}
       require 'rubygems'
       require 'rest_connection'
-      settings_accessor = Tag.connection.settings
-      settings_accessor[:user] = node[:rest_connection][:user]
-      settings_accessor[:pass] = node[:rest_connection][:pass]
-      settings_accessor[:api_url] = node[:rest_connection][:api_url]
-      settings_accessor[:common_headers] = {"X_API_VERSION"=>"1.0"}
+# incase of RACE, let's sleep randomly here lol
+      sleep rand(120)
+
+# Create the MCIs, if they don't exist.
+      if @mci_s3 = MultiCloudImageInternal.find_by(:name) {|n| n =~ /#{image_name}/ }.first
+        Chef::Log.info("Found Existing MCI with same name, re-using.. #{@mci_s3.href}")
+      else
+        @mci_s3 = MultiCloudImageInternal.create(:name => "#{image_name}", :description => "")
+      end
+
+      if @mci_ebs = MultiCloudImageInternal.find_by(:name) {|n| n =~ /#{image_name}_EBS/}.first
+        Chef::Log.info("Found Existing MCI with same name, re-using..")
+      else
+        @mci_ebs = MultiCloudImageInternal.create(:name => "#{image_name}_EBS", :description => "")
+      end
+
+      if node[:rightimage][:arch] == "i386"
+        @instance_type = "m1.small"
+      else
+        @instance_type = "m1.large"
+      end
 
       tag_these = IO.read("/var/tmp/tag_these_images.csv").split(",")
       tag_these.each do |ami|
         ami.chomp!
-        resource_href = "https://my.rightscale.com/api/acct/0/ec2_images/#{ami}?cloud_id=#{@region}"
+        resource_href = Tag.connection.settings[:api_url] + "/ec2_images/#{ami}?cloud_id=#{@region}"
         Chef::Log.info("setting image TAG for #{resource_href}")
         raise "FATAL: could not find ami, aborting." if ami.blank?
         timeout = 0
@@ -311,6 +343,7 @@ if node[:rightimage][:cloud] == "ec2"
             Tag.set(resource_href, ["provides:rs_agent_type=right_link"])
             break
           rescue => e
+            Chef::Log.info(e.to_s)
             timeout += 60
             sleep 60
             Chef::Log.info("retrying TAG for #{timeout}s")
@@ -318,6 +351,12 @@ if node[:rightimage][:cloud] == "ec2"
         end
         raise "FATAL: could not tag image after 1200 seconds. Aborting" if timeout >= 1200
       end
+
+        resource_href = Tag.connection.settings[:api_url] + "/ec2_images/#{tag_these[0]}?cloud_id=#{@region}"
+	new_setting = MultiCloudImageCloudSettingInternal.create(:multi_cloud_image_href => @mci_s3.href, :cloud_id => @region.to_i, :ec2_image_href => resource_href, :aws_instance_type => @instance_type)
+
+        resource_href = Tag.connection.settings[:api_url] + "/ec2_images/#{tag_these[1]}?cloud_id=#{@region}"
+	new_setting = MultiCloudImageCloudSettingInternal.create(:multi_cloud_image_href => @mci_ebs.href, :cloud_id => @region.to_i, :ec2_image_href => resource_href, :aws_instance_type => @instance_type)
     end
   end
 end
