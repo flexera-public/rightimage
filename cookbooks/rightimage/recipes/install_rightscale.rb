3146b0b4e5e2b6882b2fe9f9ccabe479c155d3b5
root <root@ip-10-194-98-191.ec2.internal>
2010-11-26 05:24:42 +0000

    bugfix

--- cookbooks/rightimage/recipes/install_rightscale.rb
+++ cookbooks/rightimage/recipes/install_rightscale.rb
@@ -13,8 +13,8 @@ function get_rubygems {
   mv $ROOT/tmp/rubygems-$1 $ROOT/tmp/rubygems
 }
 
-chroot $ROOT ruby --version | grep 1.8.5
-if [ $? == "0" ] ; then 
+ruby_ver=`chroot $ROOT ruby --version`
+if [[ $ruby_ver == *1.8.5* ]] ; then
   get_rubygems 1.3.3 http://rubyforge.org/frs/download.php/56227/rubygems-1.3.3.tgz
 else
   get_rubygems 1.3.7 http://rubyforge.org/frs/download.php/70696/rubygems-1.3.7.tgz
