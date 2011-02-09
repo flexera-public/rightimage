e03afce6326caf207bc257a5c040fa8a57c7c5b9
root <root@ip-10-170-189-95.us-west-1.compute.internal>
2011-02-08 21:04:47 -0500

    refs #3704 [bootstap_common] added use image_name helper in compile phase

--- cookbooks/rightimage/recipes/bootstrap_common.rb
+++ cookbooks/rightimage/recipes/bootstrap_common.rb
@@ -1,3 +1,13 @@
+class Chef::Resource::Template
+  include RightScale::RightImage::Helper
+end
+class Chef::Resource::Bash
+  include RightScale::RightImage::Helper
+end
+class Chef::Recipe
+  include RightScale::RightImage::Helper
+end
+
 # Common base image configurations 
 
 template "#{node[:rightimage][:mount_dir]}/etc/ssh/sshd_config" do
@@ -13,11 +23,11 @@ end
 ## DEBUG MODE STUFF  
 ####################
 if node[:rightimage][:debug] == "true"
-  
-  raise "ERROR: you must add 'Dev' to the image to enable debug mode" unless (image_name =~ /Dev/) != nil 
+
+  raise "ERROR: you must add 'Dev' in image name #{image_name} to enable debug mode" if (image_name !~ /Dev/) 
   
   template "#{node[:rightimage][:mount_dir]}/etc/ssh/sshd_config" do
-    only_if { node[:rightimage][:debug] == "true"  && (image_name =~ /dev/i) != nil }
+    only_if { node[:rightimage][:debug] == "true"  && (image_name !~ /Dev/)  }
     source "sshd_config.erb"
     variables({
       :permit_root_login => "yes",
@@ -26,7 +36,7 @@ if node[:rightimage][:debug] == "true"
   end
 
   bash "setup root password" do 
-    only_if { node[:rightimage][:debug] == "true"  && (image_name =~ /dev/i) != nil }
+    only_if { node[:rightimage][:debug] == "true"  && (image_name !~ /Dev/)  }
     code <<-EOH
       set -e
       set -x
