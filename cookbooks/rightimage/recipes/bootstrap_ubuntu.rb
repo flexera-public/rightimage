f12ec5b5f20569732f698b91100b81752e76579e
Jeremy Deininger <jeremy@rightscale.com>
2011-03-03 11:44:30 -0800

    [Lucid] adding debs for 302 series etc

--- cookbooks/rightimage/recipes/bootstrap_ubuntu.rb
+++ cookbooks/rightimage/recipes/bootstrap_ubuntu.rb
@@ -124,22 +124,22 @@ if node[:rightimage][:release] == "maverick" || node[:rightimage][:release] == "
 end
 
 if node[:rightimage][:release] == "lucid"
-  remote_file "/mnt/image/tmp/linux-headers-2.6.31-307_2.6.31-307.27_all.deb" do
-    source "linux-headers-2.6.31-307_2.6.31-307.27_all.deb"
+  remote_file "/mnt/image/tmp/linux-headers-2.6.31-302_2.6.31-302.7_all.deb" do
+    source "linux-headers-2.6.31-302_2.6.31-302.7_all.deb"
   end
   if node[:rightimage][:arch] == "i386"
-    remote_file "/mnt/image/tmp/linux-headers-2.6.31-307-ec2_2.6.31-307.27_i386.deb" do
-      source "linux-headers-2.6.31-307-ec2_2.6.31-307.27_i386.deb"
+    remote_file "/mnt/image/tmp/linux-headers-2.6.31-302-ec2_2.6.31-302.7_i386.deb" do
+      source "linux-headers-2.6.31-302-ec2_2.6.31-302.7_i386.deb"
     end
-    remote_file "/mnt/image/tmp/linux-image-2.6.31-307-ec2_2.6.31-307.27_i386.deb" do
-      source "linux-image-2.6.31-307-ec2_2.6.31-307.27_i386.deb"
+    remote_file "/mnt/image/tmp/linux-image-2.6.31-302-ec2_2.6.31-302.7_i386.deb" do
+      source "linux-image-2.6.31-302-ec2_2.6.31-302.7_i386.deb"
     end
   else
-    remote_file "/mnt/image/tmp/linux-headers-2.6.31-307-ec2_2.6.31-307.27_amd64.deb" do
-      source "linux-headers-2.6.31-307-ec2_2.6.31-307.27_amd64.deb"
+    remote_file "/mnt/image/tmp/linux-headers-2.6.31-302-ec2_2.6.31-302.7_amd64.deb" do
+      source "linux-headers-2.6.31-302-ec2_2.6.31-302.7_amd64.deb"
     end
-    remote_file "/mnt/image/tmp/linux-image-2.6.31-307-ec2_2.6.31-307.27_amd64.deb" do
-      source "linux-image-2.6.31-307-ec2_2.6.31-307.27_amd64.deb"
+    remote_file "/mnt/image/tmp/linux-image-2.6.31-302-ec2_2.6.31-302.7_amd64.deb" do
+      source "linux-image-2.6.31-302-ec2_2.6.31-302.7_amd64.deb"
     end
   end
   bash "install custom lucid kernel" do
