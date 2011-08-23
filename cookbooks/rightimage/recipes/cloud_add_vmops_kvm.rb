4fd11bb3c64e952049f5dc8ac5200eb936b2616b
kbockmanrs <kevin@rightscale.com>
2011-08-23 04:11:38 +0000

    [vmops_kvm] Clean up old bundled image

--- cookbooks/rightimage/recipes/cloud_add_vmops_kvm.rb
+++ cookbooks/rightimage/recipes/cloud_add_vmops_kvm.rb
@@ -237,6 +237,7 @@ bash "package image" do
     BUNDLED_IMAGE_PATH="/mnt/$BUNDLED_IMAGE"
     
     qemu-img convert -O qcow2 #{target_raw_path} $BUNDLED_IMAGE_PATH
+    [ -f $BUNDLED_IMAGE_PATH ] && rm -f $BUNDLED_IMAGE_PATH
     bzip2 $BUNDLED_IMAGE_PATH
 
   EOH
