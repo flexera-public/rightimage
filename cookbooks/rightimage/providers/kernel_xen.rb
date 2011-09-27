ebf821c7a4d6926555794c20fc993a6557da3f09
kbockmanrs <kevin@rightscale.com>
2011-09-27 23:18:11 +0000

    [Ubuntu] Support Amazon pvgrub for Lucid

--- cookbooks/rightimage/providers/kernel_xen.rb
+++ cookbooks/rightimage/providers/kernel_xen.rb
@@ -4,22 +4,31 @@ end
 
 action :install do
  
-  bash "install xen kernel" do 
+  bash "install xen kernel" do
     code <<-EOH
       set -e 
       set -x
       
       # Install to guest. 
       guest_root=#{guest_root}
-      yum -c /tmp/yum.conf --installroot=$guest_root -y install kernel-xen kmod-xfs-xen 
-      chroot $guest_root yum -y remove kernel
 
-      kernel_version=$(ls -t $guest_root/lib/modules|awk '{ printf "%s ", $0 }'|cut -d ' ' -f1-1)
- 
-      # Now rebuild ramdisk with xen drivers
-      rm -f $guest_root/boot/initrd* $guest_root/initrd*
-      chroot $guest_root mkinitrd --with=xennet --with=xenblk --with=ext3 --with=jbd --preload=xenblk -v initrd-$kernel_version $kernel_version
-      mv $guest_root/initrd-$kernel_version  $guest_root/boot/.
+      case #{node[:rightimage][:platform]} in
+        "centos")
+          yum -c /tmp/yum.conf --installroot=$guest_root -y install kernel-xen kmod-xfs-xen 
+          chroot $guest_root yum -y remove kernel
+    
+          kernel_version=$(ls -t $guest_root/lib/modules|awk '{ printf "%s ", $0 }'|cut -d ' ' -f1-1)
+     
+          # Now rebuild ramdisk with xen drivers
+          rm -f $guest_root/boot/initrd* $guest_root/initrd*
+          chroot $guest_root mkinitrd --with=xennet --with=xenblk --with=ext3 --with=jbd --preload=xenblk -v initrd-$kernel_version $kernel_version
+          mv $guest_root/initrd-$kernel_version  $guest_root/boot/.
+          ;;
+        "ubuntu")
+          chroot $guest_root apt-get -y purge linux-image-2.6.32-33-virtual
+          chroot $guest_root apt-get -y install linux-image-2.6.32-317-ec2 linux-headers-2.6.32-317-ec2 grub-legacy-ec2 
+          ;;
+        esac
     EOH
   end
  
