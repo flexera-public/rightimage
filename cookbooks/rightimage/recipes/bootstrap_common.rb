fadbef0a94bbca1cb4582fcf09ec3c91eddce596
kbockmanrs <kevin@rightscale.com>
2011-12-22 03:11:51 +0000

    bootstrap_common: Add resolv.conf

--- cookbooks/rightimage/recipes/bootstrap_common.rb
+++ cookbooks/rightimage/recipes/bootstrap_common.rb
@@ -9,6 +9,11 @@ class Chef::Recipe
 end
 
 # Common base image configurations 
+bash "resolv.conf" do
+  code <<-EOH
+    echo "nameserver 8.8.4.4" >> #{guest_root}/etc/resolv.conf
+  EOH
+end
 
 template "#{guest_root}/etc/ssh/sshd_config" do
   source "sshd_config.erb"
