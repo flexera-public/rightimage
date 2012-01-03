cb7eb40ad2f3d0db2cd9f84f195e2dee4c95a1d5
kbockmanrs <kevin@rightscale.com>
2012-01-03 22:08:42 +0000

    Move rubygems install to bootstrap_common so it is on the base image

--- cookbooks/rightimage/recipes/rightscale_install.rb
+++ cookbooks/rightimage/recipes/rightscale_install.rb
@@ -1,45 +1,5 @@
 directory "#{node[:rightimage][:mount_dir]}/etc/rightscale.d" 
 
-bash "install_rubygems" do 
-  not_if  "chroot #{node[:rightimage][:mount_dir]} which gem" 
-  code <<-EOC
-set -e
-set -x
-ROOT=#{node[:rightimage][:mount_dir]}
-
-function get_rubygems {
-  wget -O $ROOT/tmp/rubygems.tgz $2 
-  tar -xzvf $ROOT/tmp/rubygems.tgz  -C $ROOT/tmp
-  mv $ROOT/tmp/rubygems-$1 $ROOT/tmp/rubygems
-}
-
-ruby_ver=`chroot $ROOT ruby --version`
-if [[ $ruby_ver == *1.8.5* ]] ; then
-  get_rubygems 1.3.3 http://rubyforge.org/frs/download.php/56227/rubygems-1.3.3.tgz
-  # Newer versions of rake will not work with older versions of Ruby
-  rake_ver="-v 0.9.2";
-else
-  get_rubygems 1.3.7 http://rubyforge.org/frs/download.php/70696/rubygems-1.3.7.tgz
-  rake_ver="";
-fi
-
-cat <<-CHROOT_SCRIPT > $ROOT/tmp/rubygems_install.sh
-#!/bin/bash -ex
-cd /tmp/rubygems
-ruby setup.rb 
-if [ "#{node[:rightimage][:platform]}" == "ubuntu" ]; then
-  ln -sf /usr/bin/gem1.8 /usr/bin/gem
-fi
-
-gem install xml-simple net-ssh net-sftp  --no-ri --no-rdoc
-gem install rake $rake_ver --no-ri --no-rdoc
-updatedb
-CHROOT_SCRIPT
-chmod +x $ROOT/tmp/rubygems_install.sh
-chroot $ROOT /tmp/rubygems_install.sh > /dev/null 
-EOC
-end
-
 # Install rightscale package based on revision number
 if node[:rightimage][:rightlink_version] =~ /4\.[0-9]*\.[0-9]*/
   log "Building image with RunRightScripts package."
