fab1e65f00f5c7b0841093be251d86bedbc2d291
Peter Schroeter <peter.schroeter@rightscale.com>
2012-02-28 14:16:55 -0800

    fixes for openstack_upload to wrok with engr cloud, glance tool has changed a bit

--- cookbooks/rightimage/recipes/upload_openstack.rb
+++ cookbooks/rightimage/recipes/upload_openstack.rb
@@ -18,24 +18,31 @@ ruby_block "upload to cloud" do
 
     openstack_user = node[:rightimage][:openstack][:user]
     openstack_password = node[:rightimage][:openstack][:password]
-    openstack_hostname = node[:rightimage][:openstack][:hostname]
+    openstack_host = node[:rightimage][:openstack][:hostname].split(":")[0]
+    openstack_api_port = node[:rightimage][:openstack][:hostname].split(":")[1] || "5000"
+    openstack_glance_port = "9292"
 
-    Chef::Log.info("Getting openstack api token for user #{openstack_user}@#{openstack_hostname}")
-    auth_resp = `curl -d '{"auth":{"passwordCredentials":{"username": "#{openstack_user}", "password": "#{openstack_pass}"}}}' -H "Content-type: application/json" http://#{openstack_hostname}:5000/v2.0/tokens` 
-    Chef::Log.info("got response #{auth_resp}")
-    auth_data = JSON.parse(auth_resp)
+    Chef::Log.info("Getting openstack api token for user #{openstack_user}@#{openstack_host}:#{openstack_api_port}")
+    auth_resp = `curl -d '{"auth":{"passwordCredentials":{"username": "#{openstack_user}", "password": "#{openstack_password}"}}}' -H "Content-type: application/json" http://#{openstack_host}:#{openstack_api_port}/v2.0/tokens` 
+    Chef::Log.info("got response for auth req: #{auth_resp}")
+    auth_hash = JSON.parse(auth_resp)
+    access_token = auth_hash["access"]["token"]["id"]
 
-#    result = `glance-upload -A --host #{node[:rightimage][:openstack][:hostname]} --disk-format qcow2 --container-format ovf #{local_file} #{image_name}`
+    # Don't use location=file://path/to/file like you might think, thats the name of the location to store the file on the server that hosts the images, not this machine
+    cmd = %Q(glance add --auth_token=#{access_token} --url=http://#{openstack_host}:#{openstack_glance_port}/v2.0 name=#{image_name} is_public=true disk_format=qcow2 container_format=ovf < #{local_file})
+    Chef::Log.debug(cmd)
+    upload_resp = `#{cmd}`
+    Chef::Log.info("got response for upload req: #{upload_resp} to cloud.")
 
-    if result =~ /Stored image/ 
-      image_id = result.scan(/u'id':\s(\d+)/).first
+    if upload_resp =~ /added/i 
+      image_id = upload_resp.scan(/ID:\s(\d+)/i).first
       Chef::Log.info("Successfully uploaded image #{image_id} to cloud.")
       
       # add to global id store for use by other recipes
       id_list = RightImage::IdList.new(Chef::Log)
       id_list.add(image_id)
     else
-      raise "ERROR: could not upload image to cloud at #{node[:rightimage][:openstack][:hostname]} due to #{result.inspect}"
+      raise "ERROR: could not upload image to cloud at #{node[:rightimage][:openstack][:hostname]} due to #{upload_resp.inspect}"
     end
   end
 end
