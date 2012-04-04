5a7148f7469d3208cf2236f5bc1cc3dbf5e78650
Peter Schroeter <peter.schroeter@rightscale.com>
2012-04-03 17:42:06 -0700

    fixes for syntax errors, move out grub config into cloud providers

---
+++ cookbooks/rightimage/resources/image.rb
@@ -0,0 +1,6 @@
+actions :package
+
+attribute :guest_root, :kind_of => String
+attribute :platform, :kind_of => String, :equal_to => ["rhel","ubuntu","centos"]
+attribute :hypervisor, :kind_of => String, :equal_to => ["esxi","kvm","xen"], :name_attribute=>true
+attribute :image_name, :kind_of => String
