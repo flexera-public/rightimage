ddcc0f52a4b212eb38fb9d3e2e7c1c4134908ce9
kbockmanrs <kevin@rightscale.com>
2012-04-07 00:14:43 +0000

    [CentOS] Remove yum-plugin-fastestmirror on CentOS 6

--- cookbooks/rightimage/recipes/bootstrap_centos.rb
+++ cookbooks/rightimage/recipes/bootstrap_centos.rb
@@ -101,6 +101,7 @@ if [ #{node[:rightimage][:release].to_i} -lt 6 ]; then
 
   curl -o #{node[:rightimage][:mount_dir]}/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL https://fedoraproject.org/static/217521F6.txt
 else
+  chroot #{node[:rightimage][:mount_dir]} rpm -e --nodeps yum-plugin-fastestmirror
   curl -o #{node[:rightimage][:mount_dir]}/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6 https://fedoraproject.org/static/0608B895.txt
 
   # Disable ttys
