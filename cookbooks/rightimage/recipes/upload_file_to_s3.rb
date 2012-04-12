c8e2c52e0139f889c6a07eddc155782fe0ebf0e4
Peter Schroeter <peter.schroeter@rightscale.com>
2012-04-12 00:32:32 -0700

    typo

--- cookbooks/rightimage/recipes/upload_file_to_s3.rb
+++ cookbooks/rightimage/recipes/upload_file_to_s3.rb
@@ -15,7 +15,7 @@ r = gem_package "net-ssh" do
 end
 r.run_action(:install)
 
-# This is a fog dependency for version 1.13.1.  0.13.3 causes ssl connection errors, 0.13.2 seems ok. pin until its fixed
+# This is a fog dependency for version 1.3.1.  0.13.3 causes ssl connection errors, 0.13.2 seems ok. pin until its fixed
 r = gem_package "excon" do
   gem_binary "/opt/rightscale/sandbox/bin/gem"
   version "0.13.2"
@@ -26,7 +26,7 @@ Gem.clear_paths
 
 r = gem_package "fog" do
   gem_binary "/opt/rightscale/sandbox/bin/gem"
-  version "1.13.1"
+  version "1.3.1"
   action :nothing
 end
 r.run_action(:install)
