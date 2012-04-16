ed85f28b7dd5dc77b3582b89c9da8126a10e6c95
kbockmanrs <kevin@rightscale.com>
2012-04-16 22:02:06 +0000

    base_common: Use rightlink_cloud helper for symlink

--- cookbooks/rightimage/recipes/base_common.rb
+++ cookbooks/rightimage/recipes/base_common.rb
@@ -74,7 +74,7 @@ log "Add RightLink 5.6 backwards compatibility symlink"
 bash "rightlink56 symlink" do
 #  not_if "test -L #{guest_root}/var/spool/#{node[:rightimage][:cloud]}"
   code <<-EOH
-    file=/var/spool/#{node[:rightimage][:cloud]}
+    file=/var/spool/#{rightlink_cloud}
     rm -rf #{guest_root}$file
     chroot #{guest_root} ln -s /var/spool/cloud $file
   EOH
