7148fce51c21b0b4090df9e478397d312d0fcc23
Peter Schroeter <peter.schroeter@rightscale.com>
2012-04-17 13:04:58 -0700

    fix for upload

--- cookbooks/rightimage/recipes/upload_file_to_s3.rb
+++ cookbooks/rightimage/recipes/upload_file_to_s3.rb
@@ -16,9 +16,11 @@ end
 r.run_action(:install)
 
 # This is a fog dependency for version 1.3.1.  0.13.3 causes ssl connection errors, 0.13.2 seems ok. pin until its fixed
+# Use rubygems to get around mirror freeze date, for now
 r = gem_package "excon" do
   gem_binary "/opt/rightscale/sandbox/bin/gem"
   version "0.13.2"
+  source "http://rubygems.org"
   action :nothing
 end
 r.run_action(:install)
@@ -27,6 +29,7 @@ Gem.clear_paths
 r = gem_package "fog" do
   gem_binary "/opt/rightscale/sandbox/bin/gem"
   version "1.3.1"
+  source "http://rubygems.org"
   action :nothing
 end
 r.run_action(:install)
