7038c0fe0d7693853b6773b837c0a341f84ae1e7
Peter Schroeter <peter.schroeter@rightscale.com>
2012-05-04 21:59:48 -0700

    remove redundant checks

--- cookbooks/rightimage/recipes/enable_debug.rb
+++ cookbooks/rightimage/recipes/enable_debug.rb
@@ -9,29 +9,24 @@ end
 ####################
 ## DEBUG MODE STUFF  
 ####################
-if node[:rightimage][:debug] == "true"
+raise "ERROR: you must add 'Dev' in image name #{image_name} to enable debug mode" if (image_name !~ /Dev/) 
 
-  raise "ERROR: you must add 'Dev' in image name #{image_name} to enable debug mode" if (image_name !~ /Dev/) 
-  
-  template "#{guest_root}/etc/ssh/sshd_config" do
-    only_if { ((node[:rightimage][:debug] == "true") && (image_name =~ /Dev/))  }
-    source "sshd_config.erb"
-    variables({
-      :permit_root_login => "yes",
-      :password_authentication => "yes"
-    })
-  end
-
-  bash "setup root password" do 
-    only_if { ((node[:rightimage][:debug] == "true")  && (image_name =~ /Dev/))  }
-    flags "-ex"
-    code <<-EOH
-      ## set random root passwd 
-      echo 'echo root:#{generate_persisted_passwd} | chpasswd' > #{guest_root}/tmp/chpasswd
-      chmod +x #{guest_root}/tmp/chpasswd
-      chroot #{guest_root} /tmp/chpasswd
-  EOH
-  end
-
+template "#{guest_root}/etc/ssh/sshd_config" do
+  source "sshd_config.erb"
+  variables({
+    :permit_root_login => "yes",
+    :password_authentication => "yes"
+  })
 end
+
+bash "setup root password" do 
+  flags "-ex"
+  code <<-EOH
+    ## set random root passwd 
+    echo 'echo root:#{generate_persisted_passwd} | chpasswd' > #{guest_root}/tmp/chpasswd
+    chmod +x #{guest_root}/tmp/chpasswd
+    chroot #{guest_root} /tmp/chpasswd
+EOH
+end
+
 rightscale_marker :end
