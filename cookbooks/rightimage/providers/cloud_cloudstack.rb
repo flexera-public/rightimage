688f3f8a661286118c50735298eb32ee69fdd0dc
Peter Schroeter <peter.schroeter@rightscale.com>
2012-05-05 00:06:35 -0700

    use resource param

--- cookbooks/rightimage/providers/cloud_cloudstack.rb
+++ cookbooks/rightimage/providers/cloud_cloudstack.rb
@@ -175,7 +175,7 @@ action :upload do
       require "right_vmops"
       require "uri"
 
-      name = "#{image_name}_#{new_resource.hypervisor.upcase}"
+      name = "#{new_resource.image_name}_#{new_resource.hypervisor.upcase}"
       zoneId = node[:rightimage][:datacenter]
 
       case node[:rightimage][:cloudstack][:version]
@@ -224,7 +224,7 @@ action :upload do
         file_ext = "vhd.bz2"
       end
 
-      filename = "#{image_name}.#{image_file_ext}"
+      filename = "#{new_resource.image_name}.#{image_file_ext}"
       local_file = "#{temp_root}/#{filename}"
       md5sum = calc_md5sum(local_file)
 
