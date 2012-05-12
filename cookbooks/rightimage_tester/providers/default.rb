349d1d5c74ff5d131bed04e3354c8c5ac38aa13c
kbockmanrs <kevin@rightscale.com>
2012-05-12 00:03:18 +0000

    tester: Remove debug code from provider

--- cookbooks/rightimage_tester/providers/default.rb
+++ cookbooks/rightimage_tester/providers/default.rb
@@ -6,13 +6,9 @@ action :test do
         Chef::Log.info(text)
       end
 
-      logit("TEST: #{new_resource.name}\nCOMMAND: #{new_resource.command}")
+      logit("\n\nTEST: #{new_resource.name}\nCOMMAND: #{new_resource.command}")
       result = `#{new_resource.command}`
       logit("[RESULT] TEXT: #{result} CODE: #{$?.exitstatus}")
-logit($?.success?)
-logit(new_resource.fail)
-logit(new_resource.fail == true)
-logit(new_resource.fail == false)
       raise "TEST FAILED" unless ($?.success? && new_resource.fail == true) || new_resource.fail == false
 
       logit("TEST PASSED")
