836edc68d45862593841ac567c99ba908903e814
caryp <cary@rightscale.com>
2012-06-04 21:36:50 -0700

    added logic in the case enable_debug recipe is run on it's own. improved logging/comments

--- cookbooks/rightimage/recipes/enable_debug.rb
+++ cookbooks/rightimage/recipes/enable_debug.rb
@@ -6,12 +6,12 @@ class Chef::Recipe
   include RightScale::RightImage::Helper
 end
 
-####################
-## DEBUG MODE STUFF  
-####################
-raise "ERROR: you must add 'Dev' in image name #{image_name} to enable debug mode" if (image_name !~ /Dev/) 
+is_dev_image = (image_name =~ /Dev/i)
+raise "ERROR: you must add 'Dev' in image name #{image_name} to enable debug mode" unless is_dev_image
 
+log "Enable SSH password authentication and root login"
 template "#{guest_root}/etc/ssh/sshd_config" do
+  only_if { ((node[:rightimage][:debug] == "true") && is_dev_image)  }
   source "sshd_config.erb"
   backup false
   variables({
@@ -20,7 +20,9 @@ template "#{guest_root}/etc/ssh/sshd_config" do
   })
 end
 
+log "Create random root password"
 bash "setup root password" do 
+  only_if { ((node[:rightimage][:debug] == "true") && is_dev_image)  }
   flags "-ex"
   code <<-EOH
     ## set random root passwd 
