ad6ec056ec4788ffca082d8ac14ea3e027f01bb0
Peter Schroeter <peter.schroeter@rightscale.com>
2012-06-11 15:24:07 -0700

    add centos google startup scripts

--- cookbooks/rightimage/metadata.rb
+++ cookbooks/rightimage/metadata.rb
@@ -83,7 +83,7 @@ attribute "rightimage/arch",
 attribute "rightimage/cloud",
   :display_name => "Target Cloud",
   :description => "The supported cloud for the virtual image. If unset, build a generic base image.",
-  :choice => [ "ec2", "cloudstack", "eucalyptus", "openstack", "rackspace", "rackspace_managed", "azure" ],
+  :choice => [ "ec2", "cloudstack", "eucalyptus", "openstack", "rackspace", "rackspace_managed", "azure", "google"],
   :required => "recommended"
   
 attribute "rightimage/region",
--- cookbooks/rightimage/providers/cloud_google.rb
+++ cookbooks/rightimage/providers/cloud_google.rb
@@ -5,43 +5,84 @@ end
 
 action :configure do
 
-  # Add google init script
-  cookbook_file "#{guest_root}/etc/init.d/google" do
-    source "google_initscript.sh"
-    cookbook "google"
-    owner "root"
-    group "root"
-    mode "0755"
-    action :create
+  # Add google init script for ubuntu
+  if new_resource.platform == "ubuntu"
+    cookbook_file "#{guest_root}/etc/init.d/google" do
+      source "google_initscript.sh"
+      owner "root"
+      group "root"
+      mode "0755"
+      action :create
+      backup false
+    end
+  elsif new_resource.platform =~ /centos|rhel/ && new_resource.platform_version.to_f >= 6 }
+    # Add google init script for centos (6+ only)
+    cookbook_file "#{guest_root}/etc/init/google.conf" do
+      source "google.conf"
+      owner "root"
+      group "root"
+      mode "0755"
+      action :create
+      backup false
+    end
+    cookbook_file "#{guest_root}/etc/init/google_run_startup_scripts.conf" do
+      source "google_run_startup_scripts.conf"
+      owner "root"
+      group "root"
+      mode "0755"
+      action :create
+      backup false
+    end
+    cookbook_file "#{temp_root}/google_centos.tgz" do
+      source "google_centos.tgz"
+      action :create
+      backup false
+    end
+#    directory "#{guest_root}/usr/share" { recursive true }
+    bash "untar google startup scripts" do
+      code "tar zxvf #{temp_root}/google_centos.tgz -C /usr/share"
+    end
+  else
+    raise "Unsupported platform/version combination #{new_resource.platform} #{new_resource.platform_version}"
   end
+
+  
   
   # HACK: our ubuntu base images currently do not have a motd -- adding it here
-  remote_file "#{node[:rightimage][:mount_dir]}/etc/motd.tail" do 
-    source "motd" 
-    cookbook "rightimage"
+  cookbook_file "#{guest_root}/etc/motd.tail" do 
+    source "motd"
     backup false
   end
 
-  bash "Link init script to runlevels" do
-    flags "-ex" 
-    code <<-EOH
-      guest_root=#{guest_root}
-    
-      # Link init script to runlevels
-      chroot $guest_root ln -sf ../init.d/google /etc/rc0.d/K01google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc1.d/K01google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc6.d/K01google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc2.d/s99google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc3.d/s99google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc4.d/s99google
-      chroot $guest_root ln -sf ../init.d/google /etc/rc5.d/s99google
-    EOH
+  if new_resource.platform == "ubuntu"
+    bash "Link init script to runlevels" do
+      flags "-ex" 
+      code <<-EOH
+        guest_root=#{guest_root}
+      
+        # Link init script to runlevels
+        chroot $guest_root ln -sf ../init.d/google /etc/rc0.d/K01google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc1.d/K01google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc6.d/K01google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc2.d/s99google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc3.d/s99google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc4.d/s99google
+        chroot $guest_root ln -sf ../init.d/google /etc/rc5.d/s99google
+      EOH
+    end
+  else
+    # Not necessary for upstart
   end
 
   bash "configure for google compute" do
     flags "-ex" 
     code <<-EOH
       guest_root=#{guest_root}
+
+      wget http://commondatastorage.googleapis.com/pub/gsutil.tar.gz
+      tar zxvf gsutil.tar.gz -C #{guest_root}/usr/local
+      chroot $guest_root easy_install pip
+      chroot $guest_root pip install https://dl.google.com/dl/compute/gcompute.tar.gz
 
       case "#{node[:rightimage][:platform]}" in
       "centos")
@@ -50,26 +91,24 @@ action :configure do
 
       "ubuntu")
         # TOOO
-        chroot $guest_root apt-get -y install acpi dhcp3-client 
+        chroot $guest_root apt-get -y install acpi dhcp3-client
         ;;
       esac
 
       mkdir -p $guest_root/etc/rightscale.d
       echo "gc" > $guest_root/etc/rightscale.d/cloud
-
     EOH
   end
 
 end
 
 action :package do
-  rightimage_image node[:rightimage][:image_type] do
-    action :package
+  bash "zipping raw file" do 
+    code "gzip -c #{loopback_file(true)} > #{loopback_file(true)}.gz"
   end
 end
 
 action :upload do
-  
   raise "Upload not supported -- please implement me!!"
 
   ruby_block "store id" do
