3fe431a37534563508d44079435b28f1d842bcc1
kbockmanrs <kevin@rightscale.com>
2012-06-29 21:28:01 +0000

    Azure: Implement package action

--- cookbooks/rightimage/providers/image_msvhd.rb
+++ cookbooks/rightimage/providers/image_msvhd.rb
@@ -1,3 +1,19 @@
 action :package do
-# Not currently implemented.
+  execute "virtualbox install" do
+    not_if "rpm -qa VirtualBox*|grep VirtualBox"
+    command "rpm --install http://download.virtualbox.org/virtualbox/4.1.18/VirtualBox-4.1-4.1.18_78361_rhel6-1.x86_64.rpm"
+  end
+
+  bash "package image" do
+    flags "-ex"
+    cwd target_raw_root
+    code <<-EOH
+      image="#{image_name}.vhd"
+
+      echo "Remove old image"
+      rm -f $image
+
+      VBoxManage convertfromraw #{loopback_filename(partitioned?)} $image --format VHD
+    EOH
+  end
 end
