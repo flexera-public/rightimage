0c871f462b5c21f60f48a8e33591ada6cee5d054
kbockmanrs <kevin@rightscale.com>
2012-07-02 13:09:19 +0000

    Helper: image_name: On azure, substitute _ for -

--- cookbooks/rightimage/libraries/helper.rb
+++ cookbooks/rightimage/libraries/helper.rb
@@ -6,7 +6,7 @@ module RightScale
         name = node[:rightimage][:image_name].dup
         name << "_#{generate_persisted_passwd}" if node[:rightimage][:debug] == "true" && node[:rightimage][:build_mode] != "migrate" && node[:rightimage][:cloud] !~ /rackspace/
         name << "_EBS" if node[:rightimage][:ec2][:image_type] =~ /ebs/i and name !~ /_EBS/
-        name.gsub!("_","-") if node[:rightimage][:cloud] == /rackspace/
+        name.gsub!("_","-") if node[:rightimage][:cloud] == /rackspace/ || node[:rightimage][:cloud] == "azure"
         name
       end
 
