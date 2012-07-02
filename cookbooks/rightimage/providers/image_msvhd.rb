5213523e0372234a8f1dd7c71e07d1eb94fde18d
kbockmanrs <kevin@rightscale.com>
2012-07-02 13:14:35 +0000

    [Azure] package: Use yum to install VirtualBox instead of rpm, in order to install deps

--- cookbooks/rightimage/providers/image_msvhd.rb
+++ cookbooks/rightimage/providers/image_msvhd.rb
@@ -1,7 +1,7 @@
 action :package do
   execute "virtualbox install" do
     not_if "rpm -qa VirtualBox*|grep VirtualBox"
-    command "rpm --install http://download.virtualbox.org/virtualbox/4.1.18/VirtualBox-4.1-4.1.18_78361_rhel6-1.x86_64.rpm"
+    command "yum -y install http://download.virtualbox.org/virtualbox/4.1.18/VirtualBox-4.1-4.1.18_78361_rhel6-1.x86_64.rpm"
   end
 
   bash "package image" do
