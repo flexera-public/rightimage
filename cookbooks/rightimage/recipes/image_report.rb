86941ecdad3e833e7f6ec49524795c514884a31e
DrewRWx <drew@waran.is>
2012-09-01 22:36:50 +0000

    image_report: Call report_tool from the rightimage_tools gem

--- cookbooks/rightimage/recipes/image_report.rb
+++ cookbooks/rightimage/recipes/image_report.rb
@@ -8,9 +8,11 @@ class Chef::Resource::RubyBlock
   include RightScale::RightImage::Helper
 end
 
-# Stage report tool in image.
-cookbook_file "#{guest_root}/tmp/report_tool.rb" do
-  source "report_tool.rb"
+# Current rightimage_tools gem filename.
+RI_TOOL_GEM = Dir.entries(File.dirname(__FILE__)+"/../files/default/").grep(/rightimage_tools.*.gem/)
+
+# Stage rightimge_tools gem in image.
+cookbook_file "#{guest_root}/tmp/#{RI_TOOL_GEM}" do
   mode "0755"
 end
 
@@ -22,7 +24,7 @@ directory "#{guest_root}/etc/rightscale.d" do
   recursive true
 end
 
-# Provide the freeze-date and build-date to the chrooted report tool.
+# Put the freeze-date, build-date, and rightlink version in a hint file.
 ruby_block "create_hint_file" do
   block do
     hint = Hash.new
@@ -53,30 +55,36 @@ end
 bash "query_image" do
   cwd "/"
   code <<-EOH
-  # If json is not installed, install it. Otherwise, don't.
-  found="$(/usr/sbin/chroot #{guest_root} gem list json | grep -i json)"
-  # Found is nil if json wasn't installed in image.
-  if [ -z "$found" ]; then  
-    /usr/sbin/chroot #{guest_root} gem install json
+  # If rightimage_tools is not installed, install it. Otherwise, don't.
+  found="$(/usr/sbin/chroot #{guest_root} gem list rightimage_tools | grep -i rightimage_tools)"
+  # Found is nil if rightimage_tools wasn't installed in image.
+  if [ -z "$found" ]; then
+    # Stripe the version name.  
+    /usr/sbin/chroot #{guest_root} gem install /tmp/#{RI_TOOL_GEM}
     # Sentinel for uninstall at end.
     found="false"
   fi
 
   # Run report tool in image chroot.
-  # Prints report to log.
-  /usr/sbin/chroot #{guest_root} /tmp/report_tool.rb "print"
 
-  # Move json file out of image to receive md5.  
+  # Extra path for Ubuntu.
+  PATH=$PATH:/usr/local/bin
+  
+  # Prints report to log.
+  /usr/sbin/chroot #{guest_root} report_tool "print"
+
+  # Move JSON file out of image to receive MD5 checksum.
   mv #{guest_root}/tmp/report.js #{temp_root}/#{loopback_filename(partitioned?)}.js
   
-  # If json was installed, uninstall it.
+  # If rightimage_tools was installed, uninstall it.
   if [ "$found" == "false" ]; then
-    /usr/sbin/chroot #{guest_root} gem uninstall json
+    /usr/sbin/chroot #{guest_root} gem uninstall rightimage_tools
   fi
   EOH
 end
 
 # Clean up report tool.
 file "#{guest_root}/tmp/report_tool.rb" do action :delete end
+file "#{guest_root}/tmp/#{RI_TOOL_GEM}" do action :delete end
 
 rightscale_marker :end
