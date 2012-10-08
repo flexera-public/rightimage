6810c34e279490ab03d1899c448cc3602e7901a1
Peter Schroeter <peter.schroeter@rightscale.com>
2012-10-08 13:10:39 -0700

    fix previous commit

--- cookbooks/rightimage/libraries/util.rb
+++ cookbooks/rightimage/libraries/util.rb
@@ -44,7 +44,7 @@ module RightImage
       end
       # On 32 bit apt-get update fails if this directory doesn't exist
       if ::File.directory? "/var/cache/apt/archives"
-        FileUtils.mkdir("/var/cache/apt/archives/partial", :mode=>0755)
+        FileUtils.mkdir_p("/var/cache/apt/archives/partial", :mode=>0755)
       end
 
       DIRS_truncate_logs.each do |dir|
