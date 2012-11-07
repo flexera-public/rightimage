7941e0ce0b4bbd1ad33dd90377a88466010192b3
Peter Schroeter <peter.schroeter@rightscale.com>
2012-11-07 09:19:04 -0800

    Add in new datacenter (cherry picked from commit 44058710d98a359a17d4f73b9091d7102c9d04dd)
    Conflicts:
    	cookbooks/rightimage/attributes/ec2.rb

--- cookbooks/rightimage/attributes/default.rb
+++ cookbooks/rightimage/attributes/default.rb
@@ -162,6 +162,8 @@ case rightimage[:region]
     set[:rightimage][:ec2_endpoint] = "https://ec2.eu-west-1.amazonaws.com"
   when "ap-southeast"
     set[:rightimage][:ec2_endpoint] = "https://ec2.ap-southeast-1.amazonaws.com"
+  when "ap-southeast-2"
+    set[:rightimage][:ec2_endpoint] = "https://ec2.ap-southeast-2.amazonaws.com"
   when "ap-northeast"
     set[:rightimage][:ec2_endpoint] = "https://ec2.ap-northeast-1.amazonaws.com"
   when "sa-east"
@@ -258,6 +260,15 @@ when "ec2"
       set[:rightimage][:ramdisk_id] = nil
     when "x86_64"
       set[:rightimage][:aki_id] = "aki-cc3ce3d1"
+      set[:rightimage][:ramdisk_id] = nil
+    end
+  when "ap-southeast-2"
+    case rightimage[:arch]
+    when "i386"
+      set[:rightimage][:aki_id] = "aki-33990e09"
+      set[:rightimage][:ramdisk_id] = nil 
+    when "x86_64"
+      set[:rightimage][:aki_id] = "aki-31990e0b"
       set[:rightimage][:ramdisk_id] = nil
     end
   end
