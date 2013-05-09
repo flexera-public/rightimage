d4d3c4bf24f689370cfd07d4df472a47a0cabda1
kbockmanrs <kevin@rightscale.com>
2013-05-09 21:09:47 +0000

    [Vagrant][CentOS] Remove uname -r from installing kernel-devel since it isn't needed and doesn't work (acu79393)

--- cookbooks/rightimage_cloud_vagrant/templates/default/base.sh
+++ cookbooks/rightimage_cloud_vagrant/templates/default/base.sh
@@ -4,4 +4,4 @@
 # http://docs-v1.vagrantup.com/v1/docs/base_boxes.html
 sed -i "/^Defaults/ s/ requiretty/ \!requiretty/" /etc/sudoers
 # kernel headers that match the host are required for guest additions to build the kernel modules
-yum -y install dkms kernel-devel-`uname -r`
+yum -y install dkms kernel-devel
