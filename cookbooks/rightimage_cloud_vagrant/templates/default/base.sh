9b693cf3c3284c31b2de20a8dd3f4654304d5551
kbockmanrs <kevin@rightscale.com>
2013-05-09 21:07:42 +0000

    [Vagrant][CentOS] Adjust requiretty sed so you don't end up with an empty line (acu79393)

--- cookbooks/rightimage_cloud_vagrant/templates/default/base.sh
+++ cookbooks/rightimage_cloud_vagrant/templates/default/base.sh
@@ -1,5 +1,7 @@
 # Base install
 
-sed -i "s/requiretty//" /etc/sudoers
+# Disable requiretty as documented in:
+# http://docs-v1.vagrantup.com/v1/docs/base_boxes.html
+sed -i "/^Defaults/ s/ requiretty/ \!requiretty/" /etc/sudoers
 # kernel headers that match the host are required for guest additions to build the kernel modules
 yum -y install dkms kernel-devel-`uname -r`
