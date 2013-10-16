39fcf92a2e6b05830425aa68506f6e0c0eeca225
caryp <cary@rightscale.com>
2013-10-16 11:04:27 -0700

    added GCE AOSS support for EL6 and Ubuntu.  Ubuntu image is not for production workloads. Under NDA, to not merge to public repo without squash rebase

--- cookbooks/rightimage/attributes/google.rb
+++ cookbooks/rightimage/attributes/google.rb
@@ -1,1 +1,2 @@
-default_unless[:rightimage][:google][:gcutil_version] = "1.7.1"
+default_unless[:rightimage][:google][:gcutil_name] = "gcutil-1.9.1" # name of the gcutil package without and extention.
+default_unless[:rightimage][:google][:gcutil_base_url] = "https://google-compute-engine-tools.googlecode.com/files/"
