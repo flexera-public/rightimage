e40f0c8121a93cc5a211ea9106e5ba45e2947059
kbockmanrs <kevin@rightscale.com>
2013-10-18 14:26:34 +0000

    Add support for vscale cloud type (acu128450) (cherry picked from commit d423c1d0301f251eb28bee0635fa7bfd8d7b439b)

---
+++ cookbooks/rightimage/providers/cloud_vscale.rb
@@ -0,0 +1,48 @@
+class Chef::Resource
+  include RightScale::RightImage::Helper
+end
+
+
+
+action :configure do
+  rightimage_cloud "cloudstack" do
+    image_name  helper_image_name
+  
+    hypervisor        node[:rightimage][:hypervisor]
+    arch              node[:rightimage][:arch]
+    platform          node[:rightimage][:platform]
+    platform_version  node[:rightimage][:platform_version].to_f
+  
+    action :configure
+  end
+
+
+  # insert grub conf, and link menu.lst to grub.conf
+  directory "#{guest_root}/mnt/metadata" do
+    owner "root"
+    group "root"
+    mode "0750"
+    action :create
+    recursive true
+  end 
+end
+
+action :package do
+  rightimage_image node[:rightimage][:image_type] do
+    action :package
+  end
+end
+
+action :upload do
+
+  raise "Upload not yet implemented."
+
+  # add to global id store for use by other recipes
+  ruby_block "store id" do
+    block do
+    
+      id_list = RightImage::IdList.new(Chef::Log)
+      id_list.add(image_id)
+    end
+  end
+end
