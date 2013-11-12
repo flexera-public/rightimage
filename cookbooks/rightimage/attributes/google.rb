4488e42f40557486e46a3bc05ada8ca3ea6285ea
kbockmanrs <kevin@rightscale.com>
2013-11-12 16:49:10 +0000

    [Google] Update gcutil to 1.10.0 (acu134935)

--- cookbooks/rightimage/attributes/google.rb
+++ cookbooks/rightimage/attributes/google.rb
@@ -1,2 +1,2 @@
-default_unless[:rightimage][:google][:gcutil_name] = "gcutil-1.9.1" # name of the gcutil package without and extention.
+default_unless[:rightimage][:google][:gcutil_name] = "gcutil-1.10.0" # name of the gcutil package without an extention.
 default_unless[:rightimage][:google][:gcutil_base_url] = "https://google-compute-engine-tools.googlecode.com/files/"
