99defa941d62d3b41c5f38531c6124a11486a418
Peter Schroeter <peter.schroeter@rightscale.com>
2013-12-10 10:07:36 -0800

    acu120473 Bit of cleanup

--- cookbooks/rightimage/providers/hypervisor_esxi.rb
+++ cookbooks/rightimage/providers/hypervisor_esxi.rb
@@ -47,12 +47,12 @@ action :install_tools do
         cat > $guest_root/etc/yum.repos.d/vmware-tools.repo <<EOF
 [vmware-tools]
 name=VMware Tools
-baseurl=http://packages.vmware.com/tools/esx/5.5/rhel#{node[:rightimage][:platform_version].to_i}/x86_64
+baseurl=http://packages.vmware.com/tools/esx/5.0/rhel#{node[:rightimage][:platform_version].to_i}/x86_64
 enabled=1
 gpgcheck=1
 EOF
      chroot $guest_root yum -y clean all
-     chroot $guest_root yum -y install vmware-tools-esx-nox
+     chroot $guest_root yum -y install vmware-tools-esx-nox vmware-tools-esx-kmods
      rm -f $guest_root/etc/yum.repos.d/vmware-tools.repo
       ;;
 
