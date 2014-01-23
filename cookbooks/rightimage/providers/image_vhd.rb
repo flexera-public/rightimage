ede4225129df8b945ca67acd843ab971a2916cc2
kbockmanrs <kevin@rightscale.com>
2014-01-23 16:51:57 +0000

    [image_vhd] Fix xen 4.2.0 tools install, add required deps (acu145990) (cherry picked from commit f7b56bc433db823ffbeceb8ae88cf477c6055b07)
    Conflicts:
    
    	cookbooks/rightimage/providers/image_vhd.rb

--- cookbooks/rightimage/providers/image_vhd.rb
+++ cookbooks/rightimage/providers/image_vhd.rb
@@ -8,10 +8,10 @@ action :package do
 
   case node[:platform]
     when "centos", /redhat/i
-      vhd_util_deps=%w{mercurial git ncurses-devel dev86 iasl SDL python-devel libgcrypt-devel uuid-devel openssl-devel}
+      vhd_util_deps=%w{git ncurses-devel dev86 iasl SDL python-devel libgcrypt-devel uuid-devel openssl-devel glib2-devel yajl-devel texinfo}
       vhd_util_deps << (el6? ? "libuuid-devel" : "e2fsprogs-devel")
     when "ubuntu"
-      vhd_util_deps=%w{mercurial libncurses5-dev bin86 bcc iasl libsdl1.2-dev python-dev libgcrypt11-dev uuid-dev libssl-dev gettext libc6-dev libc6-dev:i386}
+      vhd_util_deps=%w{libncurses5-dev bin86 bcc iasl libsdl1.2-dev python-dev libgcrypt11-dev uuid-dev libssl-dev gettext libc6-dev libc6-dev:i386 libyajl-dev texinfo}
       vhd_util_deps << (node[:rightimage][:platform_version].to_f < 12.04 ? "libsdl1.2debian-all" : "libsdl1.2debian")
     else
       raise "ERROR: platform #{node[:platform]} not supported. Please feel free to add support ;) "
@@ -32,11 +32,12 @@ action :package do
       # Pulled from: http://bits.xensource.com/oss-xen/release/4.2.0/xen-4.2.0.tar.gz
       wget -q #{node[:rightimage][:s3_base_url]}/files/xen-4.2.0.tar.gz
       tar zxf xen-4.2.0.tar.gz
-      cd xen-4.2.0
-      patch --forward -p1 < /tmp/vhd-util-patch
-      make install-tools
+      cd xen-4.2.0/tools
+      patch --forward -p0 < /tmp/vhd-util-patch
+      cd ..
+      ./configure --disable-monitors --disable-ocamltools --disable-rombios --disable-seabios
+      make
       cd tools/blktap2/
-      make 
       make install
     EOF
   end
