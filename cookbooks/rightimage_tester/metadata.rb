c3b7856ada2f06460efcdf8201e17a870a473b3c
kbockmanrs <kevin@rightscale.com>
2014-03-11 19:41:17 +0000

    Bump rightimage_tester to 14.0.1

--- cookbooks/rightimage_tester/metadata.rb
+++ cookbooks/rightimage_tester/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 description      "A cookbook for testing RightImages"
 license          "Apache v2.0"
-version          "14.0.0"
+version          "14.0.1"
 
 depends "ros_upload"
 depends "rightscale"
