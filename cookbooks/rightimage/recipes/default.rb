b2a27ea9dcb89c8afb2b300ccc7e14f672f50afc
Peter Schroeter <peter.schroeter@rightscale.com>
2014-04-08 22:47:33 -0700

    acu159250 Fix ros_upload installation

--- cookbooks/rightimage/recipes/default.rb
+++ cookbooks/rightimage/recipes/default.rb
@@ -23,38 +23,10 @@ class Chef::Recipe
   include RightScale::RightImage::Helper
 end
 
-# Requirement for nokogiri, which is a requirement for fog and rightimage_tools
-packages = value_for_platform(
-  "ubuntu" => {"default" => %w(libxml2-dev libxslt1-dev ruby-dev)},
-  "default" => %w(libxml2-devel libxslt-devel ruby-devel)
-)
-
-packages.each do |p| 
-  package p
-end
-
 # TODO: Move host package install here instead?
 package "bzip2"
 package "gcc"
 #package "MAKEDEV" # only for Fedora?
-
-# Since we're using /usr/bin/gem below, we need to make sure it is installed
-# first, also before other include_recipes that are using /usr/bin/gem
-package "rubygems" do
-  # Hack for rbenv. You may not need the package installed.
-  not_if "which gem"
-end
-
-# Dependency of fog, v2 requires Ruby 1.9.2+
-gem_package "mime-types" do
-  version "< 2.0"
-  action :install
-end
-
-gem_package "fog" do
-  version "1.5.0"
-  action :install
-end
 
 # Load up prerequisites first
 include_recipe "rightimage_tester"
