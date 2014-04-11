7b6a26da07a224e3b885b48d1471375e9864994a
Peter Schroeter <peter.schroeter@rightscale.com>
2014-04-11 12:01:22 -0700

    acu163813 Update to tarballable cookbook

--- cookbooks/loopback_fs/metadata.rb
+++ cookbooks/loopback_fs/metadata.rb
@@ -1,5 +1,6 @@
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
+name             "loopback_fs"
 license          "All rights reserved"
 description      "Installs/Configures loopback_fs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
--- cookbooks/rightimage_cloud_vagrant/metadata.rb
+++ cookbooks/rightimage_cloud_vagrant/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 description      "Cloud provider cookbook for building RightImage vagrant boxes"
-version          "1.0.0"
+version          "13.0.0"
 
 depends "rightimage_hypervisor_virtualbox"
 depends "rightimage_image_virtualbox"
