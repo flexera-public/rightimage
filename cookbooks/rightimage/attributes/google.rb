c81444d1f9cc7527f3b256ab588700fb778a23f6
kbockmanrs <kevin@rightscale.com>
2014-04-24 16:25:03 +0000

    Google: Download gcutil and gsutil from s3 (acu163813)

--- cookbooks/rightimage/attributes/google.rb
+++ cookbooks/rightimage/attributes/google.rb
@@ -1,2 +1,2 @@
-default_unless[:rightimage][:google][:gcutil_name] = "gcutil-1.10.0" # name of the gcutil package without an extention.
-default_unless[:rightimage][:google][:gcutil_base_url] = "https://google-compute-engine-tools.googlecode.com/files/"
+default_unless[:rightimage][:google][:gcutil_name] = "gcutil-1.14.2" # name of the gcutil package without an extention.
+default_unless[:rightimage][:google][:gsutil_name] = "gsutil-3.42"
