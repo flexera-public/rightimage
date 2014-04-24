ed31585744591bc9cbfdb1c653ea32f2eff91c27
kbockmanrs <kevin@rightscale.com>
2014-04-24 16:52:49 +0000

    Bump rightimage to 13.0.1

--- cookbooks/rightimage/metadata.rb
+++ cookbooks/rightimage/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 name             "rightimage"
 description      "A cookbook for building RightImages"
-version          "13.0.0"
+version          "13.0.1"
 license          "Apache v2.0"
 
 depends "rightimage_tester"
