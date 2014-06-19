c1ecba5f5718700301c28e318dac754f93d71745
kbockmanrs <kevin@rightscale.com>
2014-06-19 00:42:52 +0000

    Create cloudstack_uploader tool (acu131763)

--- cookbooks/rightimage/metadata.rb
+++ cookbooks/rightimage/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RightScale, Inc."
 maintainer_email "support@rightscale.com"
 name             "rightimage"
 description      "A cookbook for building RightImages"
-version          "14.0.12"
+version          "14.0.13"
 license          "Apache v2.0"
 
 depends "ros_upload"
