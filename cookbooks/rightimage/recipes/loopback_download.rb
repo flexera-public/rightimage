cdc366464a414ca136dcd7d0bb62763c5cae6abd
kbockmanrs <kevin@rightscale.com>
2014-07-04 22:50:11 +0000

    Fix downloading base image from dev bucket (acu175345)

--- cookbooks/rightimage/recipes/loopback_download.rb
+++ cookbooks/rightimage/recipes/loopback_download.rb
@@ -19,18 +19,18 @@ remote_file loopback_file_compressed do
   source "http://#{image_upload_bucket}.s3.amazonaws.com/#{image_s3_path}/#{loopback_filename_compressed}"
   ignore_failure true
   backup false
-  not_if { ::File.exists?(loopback_file_base) || ::File.exists?(loopback_file_compressed) }
+  not_if { ::File.size?(loopback_file_base) || ::File.size?(loopback_file_compressed) }
   
-  action :create_if_missing
+  action :create
 end
 
 # If not in production bucket, try dev bucket
 remote_file loopback_file_compressed do
   source "http://#{image_upload_bucket}-dev.s3.amazonaws.com/#{image_s3_path}/#{loopback_filename_compressed}"
   backup false
-  not_if { ::File.exists?(loopback_file_base) || ::File.exists?(loopback_file_compressed) }
+  not_if { ::File.size?(loopback_file_base) || ::File.size?(loopback_file_compressed) }
   
-  action :create_if_missing
+  action :create
 end
 
 execute "tar xjf #{loopback_file_compressed}" do
