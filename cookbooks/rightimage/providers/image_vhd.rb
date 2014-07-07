fcfee057089b631b4e03a0b65983d99f4dc6a7c5
kbockmanrs <kevin@rightscale.com>
2014-07-07 16:49:45 +0000

    CloudStack Xen Ubuntu 14.04 support WIP (acu175345)

--- cookbooks/rightimage/providers/image_vhd.rb
+++ cookbooks/rightimage/providers/image_vhd.rb
@@ -11,7 +11,7 @@ action :package do
       vhd_util_deps=%w{git ncurses-devel dev86 iasl SDL python-devel libgcrypt-devel uuid-devel openssl-devel glib2-devel yajl-devel texinfo}
       vhd_util_deps << (el6? ? "libuuid-devel" : "e2fsprogs-devel")
     when "ubuntu"
-      vhd_util_deps=%w{libncurses5-dev bin86 bcc iasl libsdl1.2-dev python-dev libgcrypt11-dev uuid-dev libssl-dev gettext libc6-dev libc6-dev:i386 libyajl-dev texinfo}
+      vhd_util_deps=%w{libncurses5-dev bin86 bcc iasl libsdl1.2-dev python-dev libgcrypt11-dev uuid-dev libssl-dev gettext libc6-dev libc6-dev-i386 libyajl-dev texinfo}
       vhd_util_deps << (node[:rightimage][:platform_version].to_f < 12.04 ? "libsdl1.2debian-all" : "libsdl1.2debian")
     else
       raise "ERROR: platform #{node[:platform]} not supported. Please feel free to add support ;) "
@@ -28,14 +28,29 @@ action :package do
     not_if "which vhd-util"
     flags "-ex"
     code <<-EOF
+      # Ubuntu 14.04: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/778047
+      [ ! -e /usr/include/asm ] && ln -s /usr/include/asm-generic /usr/include/asm
+
       rm -rf /mnt/vhd && mkdir /mnt/vhd && cd /mnt/vhd
-      # Pulled from: http://bits.xensource.com/oss-xen/release/4.2.0/xen-4.2.0.tar.gz
+      # Pulled from: http://bits.xensource.com/oss-xen/release/4.4.0/xen-4.2.0.tar.gz
       wget -q #{node[:rightimage][:s3_base_url]}/files/xen-4.2.0.tar.gz
+      wget -q #{node[:rightimage][:s3_base_url]}/files/xen-4.2.0-patches-ubuntu-14.04.tgz
       tar zxf xen-4.2.0.tar.gz
       cd xen-4.2.0/tools
       patch --forward -p0 < /tmp/vhd-util-patch
       cd ..
       ./configure --disable-monitors --disable-ocamltools --disable-rombios --disable-seabios
+      tar zxf ../xen-4.2.0-patches-ubuntu-14.04.tgz
+      # Probably not all needed just to get vhd-util compiled
+      patch -p1 --input=patch-compat-memory
+      patch -p1 --input=patch-memset
+      patch -p0 --input=patch-qemu-xen-traditional-Makefile.target
+      patch -p0 --input=patch-tools-Makefile
+      patch -p0 --input=patch-qemu-xen-Makefile.target
+      patch -p0 --input=patch-debugger-gdbsx-xg-xg_main
+      patch -p1 --input=patch-stubdom
+      patch -p0 --input=patch-qemu-xen-traditional-xen-hooks
+      patch -p1 --input=patch-docs-man-xl.pod.1
       make
       cd tools/blktap2/
       make install
