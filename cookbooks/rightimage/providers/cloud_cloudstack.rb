e78ebe0884e79864b142fbea5a7a84e55771218f
kbockmanrs <kevin@rightscale.com>
2014-08-15 23:49:49 +0000

    [CloudStack] DHCP symlink hack for RightLink no longer needed (acu175345)

--- cookbooks/rightimage/providers/cloud_cloudstack.rb
+++ cookbooks/rightimage/providers/cloud_cloudstack.rb
@@ -10,13 +10,6 @@ action :configure do
     command "chroot #{guest_root} yum -y install iscsi-initiator-utils"
   end
 
-  Chef::Log::info "Add DHCP symlink for RightLink"
-  execute "chroot #{guest_root} ln -s /var/lib/dhcp /var/lib/dhcp3" do
-    only_if { ::File.exists?"#{guest_root}/var/lib/dhcp" }
-    creates "#{guest_root}/var/lib/dhcp3"
-  end
-
-
   bash "configure for cloudstack" do
     flags "-ex" 
     code <<-EOH
