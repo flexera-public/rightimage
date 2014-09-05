407760eb44a0672562e7ce70bfe9cc02c981020e
kbockmanrs <kevin@rightscale.com>
2014-09-05 15:12:50 +0000

    [qcow] Set compat=0.10 on full image build (acu172265)

--- cookbooks/rightimage/providers/image_qcow2.rb
+++ cookbooks/rightimage/providers/image_qcow2.rb
@@ -10,8 +10,19 @@ action :package do
       
       BUNDLED_IMAGE="#{image_name}.qcow2"
       BUNDLED_IMAGE_PATH="#{target_raw_root}/$BUNDLED_IMAGE"
+
+      set +e
+      # Detect if compat option exists. https://lists.fedoraproject.org/pipermail/virt/2014-April/004041.html
+      qemu-img create -f qcow2 -o ? blah.qcow2 | grep 'compat'
+      compat_check=$?
+      compat=""
+      set -e
+
+      if [ "$compat_check" == "0" ]; then
+        compat="-o compat=0.10"
+      fi
       
-      qemu-img convert -O qcow2 #{loopback_file} $BUNDLED_IMAGE_PATH
+      qemu-img convert -O qcow2 $compat #{loopback_file} $BUNDLED_IMAGE_PATH
 
       if [ "#{node[:rightimage][:cloud]}" != "openstack" ]; then
         [ -f $BUNDLED_IMAGE_PATH.bz2 ] && rm -f $BUNDLED_IMAGE_PATH.bz2
